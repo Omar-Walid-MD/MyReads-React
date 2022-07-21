a9d42dcdbf283cfe07d1787346efa5eaacdf3ca0
Omar-Walid-MD <omar.madinah49@yahoo.com>
2022-07-21 17:43:59 +0200

    Added ability to drag and drop books to change shelves

--- src/Book.js
+++ src/Book.js
@@ -3,18 +3,44 @@ import { update } from "./BooksAPI";
 import EditBook from "./EditBook";
 import cover from "./cover.png";
 import PropTypes from "prop-types";
+import BookDetails from "./BookDetails";
 
 //Book component that displays book properties (cover, title, authors) and edit button
-function Book({book,handle,currentShelf})
+function Book({book,handle,currentShelf,selectBook})
 {
+    function overlapping(event,b)
+    {
+        let y = b.getBoundingClientRect();
+        let top = y.top + window.pageYOffset;
+        let bottom = y.bottom + window.pageYOffset;
+
+        return (event.pageY > top && event.pageY < bottom)
+    }
+
+    function testDrag(event)
+    {
+        let allShelves = document.querySelectorAll('.book-shelf');
+
+        for(const shelf of allShelves)
+        {
+            if (overlapping(event,shelf))
+            {
+                let targetShelf = shelf.getAttribute("category");
+                console.log(targetShelf);
+                handle(book,currentShelf,targetShelf)
+            }
+        }
+    }
 
     return (
-        <div className="book-container">
-
-            <div className="book-display">
-                <img className="book-cover" src={book.imageLinks != null ? book.imageLinks.smallThumbnail : cover} alt="Book cover"/>
-                <EditBook book={book} handle={handle} currentShelf={currentShelf} />
-            </div>
+        <div className="book-container" draggable="true" onDragEnd={testDrag}>
+            <label htmlFor={"display-book-" + book.id} className="open-label">
+                <div className="book-display">
+                    <img className="book-cover" src={book.imageLinks != null ? book.imageLinks.smallThumbnail : cover} alt="Book cover"/>
+                    <EditBook book={book} handle={handle} currentShelf={currentShelf} />
+                </div>
+            </label>
+            <button className="open-book-details" id={"display-book-" + book.id} onClick={function(){selectBook(book)}}></button>
             
             <p className="book-title">{book.title}</p>
             
